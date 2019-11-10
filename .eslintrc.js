d0604772a8f02fcba971e0e2f0fe7208ab44af65
Daniel <daniel.tannor@gmail.com>
2019-11-10 19:10:19 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'airbnb-base',
+
+    env: {
+        commonjs: true,
+        node: true,
+        mocha: true
+    },
+    rules: {
+        indent: ['error', 4]
+    }
+};
